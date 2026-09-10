578c7351790147eee924726169c688bcef199719
mdrazzakhosen009-cpu <mdrazzakhosen009@gmail.com>
2026-09-10 13:34:49 +0600

    Delete scripts/init-dblll.js

--- scripts/init-dblll.js
+++
@@ -1,3 +0,0 @@
-require("dotenv").config();
-const { initDb } = require("../src/db");
-initDb().then(()=>{ console.log("Turso database initialized."); process.exit(0); }).catch(e=>{ console.error(e); process.exit(1); });
